7d718252039b81c43a09ea5ae330305dfd5bddca
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-08-27 13:26:20 +0530

    Initial Commit

---
+++ src/main/java/com/example/DoctorHub/DoctorHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoctorHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoctorHub/DoctorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoctorHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
